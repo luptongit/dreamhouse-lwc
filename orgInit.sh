d7701afe4d5287de4718b6ab0207bae112bace91
Jonathan Lupton <jlupton@salesforce.com>
2019-10-22 16:02:48 -0400

    Added orgInit for workshop deployability

---
+++ orgInit.sh
@@ -0,0 +1,6 @@
+sfdx force:org:create -f config/project-scratch-def.json -d 7 -s -w 60 -a VistaprintPlatformWorkshop
+
+sfdx force:source:push
+sfdx force:user:permset:assign -n dreamhouse
+sfdx force:data:tree:import --plan data/sample-data.json
+sfdx force:org:open 
