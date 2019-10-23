2cde3d4e6302ea5ea7d1d8710b43bc365e43a929
Jonathan Lupton <jlupton@salesforce.com>
2019-10-22 22:24:59 -0400

    Fixed Errors on Push Made "Flow Dynamic" Added Vista Permission Set Process Builder - Record Type - simplified

--- orgInit.sh
+++ orgInit.sh
@@ -2,5 +2,6 @@ sfdx force:org:create -f config/project-scratch-def.json -d 7 -s -w 60 -a Vistap
 
 sfdx force:source:push
 sfdx force:user:permset:assign -n dreamhouse
+sfdx force:user:permset:assign -n vistaprint
 sfdx force:data:tree:import --plan data/sample-data.json
 sfdx force:org:open 
